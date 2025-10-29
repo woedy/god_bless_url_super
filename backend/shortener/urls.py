00ab0ddab3ca27ca267299e184d4336e624ab1d5
SamaWyna <etornamasamoah@gmail.com>
2025-10-29 15:39:04 +0000

    Add frontend delete action for shortened links

--- backend/shortener/urls.py
+++ backend/shortener/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import BulkCreateLinksView, LinkListView, LinkStatsView
+from .views import BulkCreateLinksView, LinkDetailView, LinkListView, LinkStatsView
 
 urlpatterns = [
     path("links/", LinkListView.as_view(), name="link-list"),
     path("links/bulk/", BulkCreateLinksView.as_view(), name="link-bulk-create"),
+    path("links/<slug:code>/", LinkDetailView.as_view(), name="link-detail"),
     path("links/<slug:code>/stats/", LinkStatsView.as_view(), name="link-stats"),
 ]
